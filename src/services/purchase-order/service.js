8c2264fe9ad3af1f9348de733828102e5cf17613
Fathan <mahendra_fathan@linkaja.id>
2022-03-17 12:25:46 +0700

    fix get company id

--- src/services/purchase-order/service.js
+++ src/services/purchase-order/service.js
@@ -88,10 +88,17 @@ class PurchaseOrderService {
     const body = req.body
     try {
       const user = await request('GET', COMPANY_API, '', '', authorize)
-      const company = CompanyDto
+      const company = CompanyDto      
+      if (!company.data) {
+        res.status(404)
+        return res.send(errorResponse(404, 'company tidak ditemukan'))
+      }
       company.id = user.data._id
       company.image = user.data.image
       company.name = user.data.name
+      company.email = user.data.email
+      company.phone_number = user.data.phone
+      company.contact = user.data.pic_name
       const items = []
       body.items.forEach(value => {
         const item = new ItemDto()
